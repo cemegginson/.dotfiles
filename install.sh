ce080f65154070fe8c5e33cfdd7ce88ab4496d0a
Casey Megginson <tenncasey@gmail.com>
2016-03-26 11:50:13 -0500

    Added line to copy zshrc to roots' home directory.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 cp configs/vimrc ~/.vimrc
 cp configs/yaourtrc ~/.yaourtrc
 cp configs/zshrc ~/.zshrc
+sudo cp configs/zshrc /root/.zshrc
 
 # Copy zsh theme
 sudo cp configs/caseymegginson.zsh-theme /usr/share/oh-my-zsh/themes/
