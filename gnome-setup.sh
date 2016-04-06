3e2662890b28290b8bededde94ee9170945be166
Casey Megginson <tenncasey@gmail.com>
2016-04-06 00:11:38 -0500

    Added quick and dirty setup script for gnome-shell extensions.

---
+++ gnome-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir gnome-extensions
+cp -r submodules/gnome-shell-system-monitor-applet/system-monitor@paradoxxx.zero.gmail.com gnome-extensions/
+cp -r submodules/multi-monitors-add-on/multi-monitors-add-on@spin83 gnome-extensions/
+cp -r submodules/gse-sound-output-device-chooser/sound-output-device-chooser@kgshank.net gnome-extensions/
+
+rm -rf ~/.local/share/gnome-shell/extensions
+
+ln -s $PWD/gnome-extensions ~/.local/share/gnome-shell/extensions
