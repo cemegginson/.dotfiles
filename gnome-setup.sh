02e676374edb1bf74a14938f378a709123ad9ef2
Casey Megginson <tenncasey@gmail.com>
2016-04-23 14:25:47 -0500

    Added Backslide.

--- gnome-setup.sh
+++ gnome-setup.sh
@@ -6,6 +6,7 @@ mkdir gnome-extensions
 cp -r submodules/gnome-shell-system-monitor-applet/system-monitor@paradoxxx.zero.gmail.com gnome-extensions/
 cp -r submodules/multi-monitors-add-on/multi-monitors-add-on@spin83 gnome-extensions/
 cp -r submodules/gse-sound-output-device-chooser/sound-output-device-chooser@kgshank.net gnome-extensions/
+cp -r submodules/backslide/backslide@codeisland.org gnome-extensions/
 cp -r submodules/nohotcorner gnome-extensions/nohotcorner@azuri.free.fr
 
 rm -rf ~/.local/share/gnome-shell/extensions
